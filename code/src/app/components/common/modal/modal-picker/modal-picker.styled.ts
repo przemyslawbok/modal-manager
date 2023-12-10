f3e88946de16776e70fefd023a87a569e2cd9ad2
Jules Caesaris <julescaesaris@gmail.com>
2023-12-10 13:38:40 +0100

    Fix bug fetching title from config, styling adjustments

--- code/src/app/components/common/modal/modal-picker/modal-picker.styled.ts
+++ code/src/app/components/common/modal/modal-picker/modal-picker.styled.ts
@@ -1,23 +1,25 @@
 import styled from '@emotion/styled'
 import { Box } from '@mui/material'
 
-export const CenteredModalBox = styled(Box)`
+const CommonBox = styled(Box)`
+  z-index: 1;
+  padding: 1.5rem;
+`
+
+export const CenteredModalBox = styled(CommonBox)`
   width: 400px;
   height: 400px;
   border: 1px solid black;
-  padding: 1.5rem;
 `
 
-export const RightModalBox = styled(Box)`
+export const RightModalBox = styled(CommonBox)`
   width: 600px;
   height: 100%;
-  border: 1px solid black;
-  padding: 1.5rem;
+  border-left: 1px solid black;
 `
 
-export const BottomModalBox = styled(Box)`
+export const BottomModalBox = styled(CommonBox)`
   width: 100%;
   height: 200px;
-  border: 1px solid black;
-  padding: 1.5rem;
+  border-top: 1px solid black;
 `
--- code/src/app/use-modal/utils/get-params-object.ts
+++ code/src/app/use-modal/utils/get-params-object.ts
@@ -1,6 +1,9 @@
 import { ModalParams, ModalType } from '../data';
 
 export const getParamsObject = (modalType: ModalType, configs: ModalParams[], currentView?: string) => {
-  const config = configs.find(({type}) => type === modalType) ?? {}
-  return { ...config, currentView: currentView, title: config.views ? config.views[0].title : config.title };
+  const config = configs.find(({type}) => type === modalType) ?? {};
+  const viewTitle = currentView && config.views ? config.views.find(({view}) => view === currentView)?.title : undefined;
+  const defaultTitle = config.views ? config.views[0].title : config.title;
+
+  return { ...config, currentView, title: viewTitle || defaultTitle };
 }
