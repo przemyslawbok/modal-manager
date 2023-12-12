6a66a674dd4d6f8598af52b93a150869f8085b7f
Jules Caesaris <julescaesaris@gmail.com>
2023-12-12 20:10:31 +0100

    Tested how easy it is to add new Create modal

--- code/src/components/modal-manager/modals/create-modal/create-modal.tsx
+++ code/src/components/modal-manager/modals/create-modal/create-modal.tsx
@@ -1,9 +1,12 @@
 import { Modal } from '@/components/common/modal';
+import { useModalContext } from '@/contexts/use-modal';
 import { FC } from 'react';
 
 export const CreateModal: FC = () => {
+  const { isCreateOpen } = useModalContext();
+
   return (
-    <Modal title='Create modal' isOpen={false}>
+    <Modal title='Create modal' isOpen={isCreateOpen}>
       Create modal
     </Modal>
   );
