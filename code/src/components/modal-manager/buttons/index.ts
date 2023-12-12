96cd582aa077d3f78e33cd93d9b318d8c124b17a
Jules Caesaris <julescaesaris@gmail.com>
2023-12-12 20:00:22 +0100

    Added Create components to barrel files

--- code/src/components/modal-manager/buttons/index.ts
+++ code/src/components/modal-manager/buttons/index.ts
@@ -1,7 +1,8 @@
 import { AddMoodboardsButton } from './add-moodboards-button'
 import { AddResourcesButton } from './add-resources-button'
+import { CreateButton } from './create-button'
 import { EditButton } from './edit-button'
 import { ShowDevelopmentsButton } from './show-developments-button'
 import { ShowInspirationButton } from "./show-inspiration-button"
 
-export { ShowInspirationButton, AddResourcesButton, AddMoodboardsButton, ShowDevelopmentsButton, EditButton }
+export { ShowInspirationButton, AddResourcesButton, AddMoodboardsButton, ShowDevelopmentsButton, EditButton, CreateButton }
