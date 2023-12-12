9fa5c250d79934c6be0cef8f0e0446e518318cea
Jules Caesaris <julescaesaris@gmail.com>
2023-12-12 20:00:22 +0100

    Added Create components to barrel files

--- code/src/components/modal-manager/modals/index.ts
+++ code/src/components/modal-manager/modals/index.ts
@@ -1,5 +1,6 @@
+import { CreateModal } from './create-modal'
 import { DevelopmentsModal } from './developments-modal'
 import { EditModal } from './edit-modal'
 import { InspirationModal } from './inspiration-modal'
 
-export { EditModal, InspirationModal, DevelopmentsModal }
+export { EditModal, InspirationModal, DevelopmentsModal, CreateModal }
